25d329df3aeb271d5e4da94d2552eac71b9e5cca
Tangel02 <angel.adrian.moguel@gmail.com>
2025-12-04 02:32:22 -0600

    Build listo para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/Frontend-Vue-agenda-contactos/', // <-- nombre EXACTO del repo
   plugins: [vue()],
-  base: './'
-})
+});
