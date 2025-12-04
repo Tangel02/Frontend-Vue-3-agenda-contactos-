36fb7b42b1800721ff929dd70bbeae2c8c256ffc
Tangel02 <angel.adrian.moguel@gmail.com>
2025-12-04 02:42:37 -0600

    Build para GitHub Pages en /docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Frontend-Vue-agenda-contactos/', // <-- nombre EXACTO del repo
+  base: '/Frontend-Vue-agenda-contactos/', // nombre EXACTO del repo en GitHub
+  build: {
+    outDir: 'docs', // 👈 aquí le decimos a Vite que use /docs en vez de /dist
+  },
   plugins: [vue()],
-});
+})
